a7ee1cf540111b4c8998ed613be6e7c18feb00e5
andrewozhegov <andrew.ozhegov@yandex.ru>
2019-03-11 15:34:26 +0300

    separate current future and done words lists

--- tools/words.sh
+++ tools/words.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-exec &>/dev/null
+#exec &>/dev/null
+exec &>"$(dirname `readlink -e "$0"`)/words.log"
 set -x
 
 DB="$(dirname `readlink -e "$0"`)/words.db"
 DB_DONE="$(dirname `readlink -e "$0"`)/done.db"
-TIMESPAN=5
-
-function update_conf
-{
-    WORDS_COUNT="$( wc -l "${DB}" | awk '{print $1}' )"
-    VALUES_SUM="$( cat "${DB}" | awk '{sum += $1} END {print sum}' )"
-    sort -nr -k 1 -o "${DB}" "${DB}"
-}
+DB_QUEUE="$(dirname `readlink -e "$0"`)/queue.db"
+TIMESPAN=300
+INVOLVED_WORDS_N=20
 
 function ask_word
 {
@@ -23,6 +19,35 @@ function ask_word
     [ "${ANSW}" == "$(kdialog --inputbox "Translate '$WORD':")" ]
 }
 
+function get_priority
+{
+    echo $( awk 'NR == '$1' {print $1}' "${DB}" )
+}
+
+function increase_priority
+{
+    local PRIORITY_N=`get_priority "$1"`
+    sed -i ''$1's/'$PRIORITY_N'/'`expr $PRIORITY_N + $2`'/' "${DB}"
+}
+
+function add_from_queue
+{
+    cat "${DB_QUEUE}" | awk 'NR == 1' >> ${DB}
+    sed -i '1d' "${DB_QUEUE}"
+}
+
+function update_conf
+{
+    while true ; do
+        WORDS_COUNT="$( wc -l "${DB}" | awk '{print $1}' )"
+        add_from_queue
+        [ "$WORDS_COUNT" -ge "$INVOLVED_WORDS_N" ] && break
+    done
+
+    VALUES_SUM="$( cat "${DB}" | awk '{sum += $1} END {print sum}' )"
+    sort -nr -k 1 -o "${DB}" "${DB}"
+}
+
 while true
 do
     update_conf
@@ -31,17 +56,17 @@ do
     [ "$RAND" -eq 0 ] && continue
 
     for ((N=1, PRIORITY_N=0, PRIORITY_FULL=0; N <= ${WORDS_COUNT}; N++)) ; do
-        PRIORITY_N="$( awk 'NR == '${N}' {print $1}' "${DB}" )"
+        PRIORITY_N=`get_priority "$N"`
         PRIORITY_FULL=`expr $PRIORITY_FULL + $PRIORITY_N`
         [ "$RAND" -le "$PRIORITY_FULL" ] && break
     done
 
     ask_word "$N" && {
-        sed -i ''$N's/'$PRIORITY_N'/'`expr $PRIORITY_N - 1`'/' "${DB}"
+        increase_priority "$N" "-1"
         [ "$PRIORITY_N" -eq 1 ] && {
             awk 'NR == '$N' {print $0}' "${DB}" >> "${DB_DONE}"
             sed -i ''$N'd' "${DB}"
         }
         sleep "$TIMESPAN"
-    } || sed -i ''$N's/'$PRIORITY_N'/'`expr $PRIORITY_N + 4`'/' "${DB}"
+    } || increase_priority "$N" "4"
 done
